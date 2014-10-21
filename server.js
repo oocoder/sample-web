efe0507ae7b2fa42638c768df6ee83a3efef5a7a
alex <calidadis@gmail.com>
2014-10-21 17:48:58 -0400

    change default web port 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js - simple node.js web server
 var http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const ADDRESS = '0.0.0.0';
 
 var server = http.createServer(function(req, res){
@@ -12,7 +12,7 @@ var server = http.createServer(function(req, res){
 server.listen(PORT, ADDRESS, function(){
     console.log('Server running at http://%s:%d/', ADDRESS, PORT);
     console.log('Press CTRL+C to exit');
-
+    
     // Check if we are running as root
     if (process.getgid && process.getgid() === 0) {
         process.setgid('nobody');
