4e19875dcaeff4e34e2cbdfb48dbe30209c0813f
alex <calidadis@gmail.com>
2014-10-21 03:38:28 -0400

    update server to remove preivilages

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const ADDRESS = '0.0.0.0';
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');    
+    res.end(req.headers);    
 });
 
 server.listen(PORT, ADDRESS, function(){
     console.log('Server running at http://%s:%d/', ADDRESS, PORT);
     console.log('Press CTRL+C to exit');
+
+    // Check if we are running as root
+    if (process.getgid && process.getgid() === 0) {
+        process.setgid('nobody');
+        process.setuid('nobody');
+    }    
 });
 
 process.on('SIGTERM', function () {
