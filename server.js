1821e80f408ad128b6abd79fcd13b5f499b7237d
alex <calidadis@gmail.com>
2014-11-07 16:08:27 -0500

    updated to test webhooks

--- server.js
+++ server.js
@@ -1,20 +1,64 @@
 // server.js - simple node.js web server
-var http = require('https')
-    , fs = require('fs');
+var http = require('http');
+var url = require('url');
+var query = require('querystring');
 
-var options = {
-    key: fs.readFileSync('./keys/domain.key'),
-    cert: fs.readFileSync('./keys/domain.crt')
-};   
+const webHookId = "4455661234";
+
+// var fs = require('fs');
+// var options = {
+    // key: fs.readFileSync('./keys/domain.key'),
+    // cert: fs.readFileSync('./keys/domain.crt')
+// };   
     
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 80;
 const ADDRESS = '0.0.0.0';
-
-var server = http.createServer(options, function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+var fullBody = "";
+var server = http.createServer(function(req, res){
+    // We are only handling post methods for now
+    if(req.method !== 'POST'){
+        console.error("[405] " + req.method + " to " + req.url);
+        res.writeHead(405, "Method not supported", {'Content-Type': 'text/plain'});
+        res.end('405 - Method not supported');
+        return;
+    }
     
-    res.end(JSON.stringify(req.headers));    
+    req.setEncoding('utf8');
+    req.on('data', function( chunk ){ fullBody+=chunk });
+    req.on('end', function(){
+        var query = url.parse(req.url, true);
+                
+        // Call Build Handler 
+        if(query.path == '/build'){
+            HandleBuildReq(fullBody);
+        }
+        
+        // Return 'OK'
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        //JSON.stringify(req.headers)
+        res.end(fullBody);
+        
+    });
+    
 });
+
+function HandleBuildReq( body ){
+    var data = JSON.parse(body);
+
+    //TODO: Need to find a good url parser regex
+    var projurl = data.repository.absolute_url.split('/');
+    var projname = projurl[projurl.length-1] == '' ? projurl[projurl.length-2] : 
+        projurl[projurl.length-1];    
+    
+    console.log('building project:', projname );
+    
+    // var buildProjFunc = BuildProjectFunc.create(projname);
+    
+    // buildProjFunc.run(function( err, results ){
+        // if(err) return console.error('Error while running build.', err.message);
+    // });
+}
+
 
 server.listen(PORT, ADDRESS, function(){
     console.log('Server running at http://%s:%d/', ADDRESS, PORT);
