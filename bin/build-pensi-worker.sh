dda7c5083c65c1a39703483936892d434ef16c84
alex <calidadis@gmail.com>
2014-11-13 21:25:44 -0500

    updated build script

--- bin/build-pensi-worker.sh
+++ bin/build-pensi-worker.sh
@@ -31,7 +31,8 @@ var tag = "";
 cmdQueue.push(function(cb){
     try{
         var pkg = require(path.join(workPath, source, 'package.json'));
-        tag = util.format('%s/%s', source, pkg.version.replace(/\./g, '_'));
+        tag = util.format('%s/%s', source.replace(/\./g, '_').replace(/-/g, '_'), 
+            pkg.version);
         cb(null, {tag: tag, code: 0});
     } catch(e) { cb(e) }
 });
